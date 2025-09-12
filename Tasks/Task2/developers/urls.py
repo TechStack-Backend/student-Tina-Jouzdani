cf4cea0648c49b1cba653c19c92a57822a1759fe
Tina Jouzdani <tinajouzdani@gmail.com>
2025-09-12 16:22:46 +0330

    feat(Task-2): working on tamplates

--- Tasks/Task2/developers/urls.py
+++ Tasks/Task2/developers/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("developer/", views.developers_list_views,name="developers_list"),
-    path("developer/<str:username>", views.developers_cv_views,name="developers_cv"),
+    path("developer/", views.developers_list_view,name="developers_list"),
+    path("developer/<str:username>", views.developer_cv_view,name="developers_cv"),
 ]
