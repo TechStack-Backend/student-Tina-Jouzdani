c74064827befcb690107459f2f4c77a1f86f5e98
Tina Jouzdani <tinajouzdani@gmail.com>
2025-09-19 16:35:44 +0330

    fix(Task-2):Resolve comments

--- Tasks/Task2/developers/urls.py
+++ Tasks/Task2/developers/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("developer/", views.developers_list_view,name="developers_list"),
-    path("developer/<str:username>", views.developer_cv_view,name="developers_cv"),
+    path("developers/", views.developers_list_view,name="developers_list"),
+    path("developers/<str:username>", views.developer_cv_view,name="developers_cv"),
 ]
--- Tasks/Task2/developers/views.py
+++ Tasks/Task2/developers/views.py
@@ -1,33 +1,31 @@
 from django.shortcuts import render
-
-developers_list = [
-    {
+from django.http import Http404
+developers_dict = {
+    "tinatou": {
         'first_name': 'Tina',
         'last_name': 'Jouzdani',
-        'username': 'tinatuo',
-        'skills': ['Python', 'Django', 'httml']
+        'skills': ['Python', 'Django', 'HTML']
     },
-    {
+    "ali123": {
         'first_name': 'Ali',
         'last_name': 'Rezaei',
-        'username': 'ali123',
         'skills': ['Python', 'Django', 'JavaScript']
     },
-    {
+    "sara456": {
         'first_name': 'Sara',
         'last_name': 'Ahmadi',
-        'username': 'sara456',
         'skills': ['Java', 'Spring', 'SQL']
-    },
-]
+    }
+}
+
 
 def developers_list_view(request):
-    return render(request, 'developers/developers_list.html', {'developers': developers_list})
+    return render(request, 'developers/developers_list.html', {'developers': developers_dict})
+
 
 
 def developer_cv_view(request, username):
-    developer = next((dev for dev in developers_list if dev['username'] == username), None)
+    developer = developers_dict.get(username)
     if not developer:
-        from django.http import Http404
         raise Http404("Developer not found")
     return render(request, 'developers/developers_cv.html', {'developer': developer})
