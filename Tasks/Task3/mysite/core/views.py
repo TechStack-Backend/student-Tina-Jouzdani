b0f33c0d4273b6d96ae7cc4e63009fffb752ee50
Tina Jouzdani <tinajouzdani@gmail.com>
2025-09-28 11:36:03 +0330

    feat(Task-3): creat and work on views

--- Tasks/Task3/mysite/core/views.py
+++ Tasks/Task3/mysite/core/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Developer, Project
+from .forms import DeveloperForm, ProjectForm
 
-# Create your views here.
+def developers_list(request):
+    developers = Developer.objects.prefetch_related('skills').all()
+    return render(request, 'core/developers_list.html', {'developers': developers})
+
+def projects_list(request):
+    projects = Project.objects.prefetch_related('developers').all()
+    return render(request, 'core/projects_list.html', {'projects': projects})
+
+def developer_create(request):
+    if request.method == 'POST':
+        form = DeveloperForm(request.POST)
+        if form.is_valid():
+            dev = form.save()
+            return redirect('developers_list')
+    else:
+        form = DeveloperForm()
+    return render(request, 'core/developer_form.html', {'form': form})
+
+def project_create(request):
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.save()
+            form.save_m2m()
+            return redirect('projects_list')
+    else:
+        form = ProjectForm()
+    return render(request, 'core/project_form.html', {'form': form})
